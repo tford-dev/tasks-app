e42f488a902cbafe5d53ac806cf7921133310bea
tford-dev <terranceford777@gmail.com>
2025-02-28 15:48:14 -0500

    Added code to UserSignOut to make UserSignOut a variable. seems to have fixed the issue without the DOM reflecting a user signing out

--- src/Components/UserSignOut.js
+++ src/Components/UserSignOut.js
@@ -1,13 +1,28 @@
-/* eslint-disable */ 
-import React, {useEffect} from 'react';
-import { Redirect } from 'react-router-dom';
+// /* eslint-disable */ 
+// import React, {useEffect} from 'react';
+// import { Redirect } from 'react-router-dom';
+// import { useStateValue } from '../ContextApi/StateProvider';
+
+// //stateless component with an anonymous function to sign user out and redirect them to "/" route
+// export default () => {
+// 	const [initialState, dispatch] = useStateValue();
+// 	useEffect(()=> initialState.signOut());
+// 	return (
+// 		<Redirect to="/signin" />
+// 	);
+// }
+
+/* eslint-disable */
+import React, { useEffect } from 'react';
 import { useStateValue } from '../ContextApi/StateProvider';
 
-//stateless component with an anonymous function to sign user out and redirect them to "/" route
-export default () => {
-	const [initialState, dispatch] = useStateValue();
-	useEffect(()=> initialState.signOut());
-	return (
-		<Redirect to="/signin" />
-	);
+export default function UserSignOut() {
+    const [initialState, dispatch] = useStateValue();
+
+    useEffect(() => {
+        initialState.signOut();
+        window.location.href = "/signin"; // Force a full page reload
+    }, []);
+
+    return null; // Prevent React from rendering anything
 }
